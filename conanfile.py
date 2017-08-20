4774712bc26a1a2f1faf3c519f43b95b1912bb3a
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:44 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostCircular_BufferConan(ConanFile):
     name = "Boost.Circular_Buffer"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-circular_buffer"
     source_url = "https://github.com/boostorg/circular_buffer"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -24,7 +25,7 @@ class BoostCircular_BufferConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):

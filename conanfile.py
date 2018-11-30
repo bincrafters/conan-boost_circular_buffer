cbfcaa90b9919f1210192c9840b7c4a7e3770faa
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 21:59:08 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostCircular_BufferConan(base.BoostBaseConan):
     name = "boost_circular_buffer"
@@ -24,5 +24,3 @@ class BoostCircular_BufferConan(base.BoostBaseConan):
         "boost_type_traits",
         "boost_utility"
     ]
-
-
